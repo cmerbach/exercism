ff610dfcae0fefbb670ba89fa1c1cff56aff17f5
cmerbach <christian.merbach@exb.de>
2025-05-24 17:04:19 +0200

    feat(exercise): track 004 - solved

--- go/004_weather-forecast/weather_forecast.go
+++ go/004_weather-forecast/weather_forecast.go
@@ -1,8 +1,12 @@
+// Package weather provides the weather.
 package weather
 
+// CurrentCondition returns a string.
 var CurrentCondition string
+// CurrentLocation return a string.
 var CurrentLocation string
 
+// Forecast is a function what gives back some information.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
