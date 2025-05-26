26ae9aa8d13fea5798c5c47697e64a324fa4b84f
cmerbach <christian.merbach@exb.de>
2025-05-26 15:12:14 +0200

    feat(exercise): track 016 - solved

--- go/016_chessboard/chessboard.go
+++ go/016_chessboard/chessboard.go
@@ -1,27 +1,77 @@
 package chessboard
 
 // Declare a type named File which stores if a square is occupied by a piece - this will be a slice of bools
+type File []bool
 
 // Declare a type named Chessboard which contains a map of eight Files, accessed with keys from "A" to "H"
+type Chessboard map[string]File
 
 // CountInFile returns how many squares are occupied in the chessboard,
 // within the given file.
 func CountInFile(cb Chessboard, file string) int {
-	panic("Please implement CountInFile()")
+	fileSlice, exists := cb[file]
+	if !exists {
+		return 0 // File !exist
+	}
+	
+	count := 0
+	for _, occupied := range fileSlice {
+		if occupied {
+			count++
+		}
+	}
+	
+	return count
 }
 
 // CountInRank returns how many squares are occupied in the chessboard,
 // within the given rank.
 func CountInRank(cb Chessboard, rank int) int {
-	panic("Please implement CountInRank()")
+	if rank < 1 || rank > 8 {
+		return 0 // Rank !exist
+	}
+	
+	rankIndex := rank - 1
+	count := 0
+	for _, occupied := range cb {
+		if occupied[rankIndex] {
+			count++
+		}
+	}
+	
+	return count
 }
 
 // CountAll should count how many squares are present in the chessboard.
 func CountAll(cb Chessboard) int {
-	panic("Please implement CountAll()")
+	files := 0
+	ranks := 0
+	
+	// files
+	for range cb {
+		files++
+	}
+	
+	// ranks
+	for _, file := range cb {
+		ranks = len(file)
+		break
+	}
+	
+	return files * ranks
 }
 
 // CountOccupied returns how many squares are occupied in the chessboard.
 func CountOccupied(cb Chessboard) int {
-	panic("Please implement CountOccupied()")
+	count := 0
+   
+	for _, file := range cb {
+		for _, occupied := range file {
+			if occupied {
+				count++
+			}
+		}
+	}
+	
+	return count
 }
