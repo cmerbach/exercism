6fdb0ce2b232601b2ea8cd60b1b2eedca7c3b590
cmerbach <christian.merbach@exb.de>
2025-05-30 17:21:00 +0200

    feat(exercise): track 026 - solved

--- go/026_animal-magic/animal_magic.go
+++ go/026_animal-magic/animal_magic.go
@@ -1,16 +1,27 @@
 package chance
 
+import (
+	"math/rand"
+)
+
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
-	panic("Please implement the RollADie function")
+	return rand.Intn(20) + 1
 }
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	return rand.Float64() * 12.0
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
 func ShuffleAnimals() []string {
-	panic("Please implement the ShuffleAnimals function")
+	a := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+
+ 	// shuffling the slice
+	rand.Shuffle(len(a), func(i, j int) {
+		a[i], a[j] = a[j], a[i]
+	})
+
+	return a
 }
