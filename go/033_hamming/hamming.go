71377688f1d4c79f7e201425f625c04225356e93
cmerbach <christian.merbach@exb.de>
2025-06-10 11:23:44 +0200

    feat(exercise): track 033 - solved

---
+++ go/033_hamming/hamming.go
@@ -0,0 +1,21 @@
+package hamming
+
+import (
+	"errors"
+)
+
+// Distance calculates the Hamming distance between two DNA strands
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("strands must be of equal length")
+	}
+
+	distance := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+	
+	return distance, nil
+}
